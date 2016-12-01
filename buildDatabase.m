2e0285a2b70ad212d982d8d1bd83edebf201d5b7
Walker Sensabaugh <walkersens@gmail.com>
2016-11-30 21:00:02 -0500

    Added the ability to crop by circles

--- buildDatabase.m
+++ buildDatabase.m
@@ -2,9 +2,8 @@ function db = buildDatabase(detector)
 %BUILDDATABASE Build database of quarters
 
 db = QuarterDatabase();
-dbFolder = 'GoodLight';
+dbFolder = '../quarters/GoodLight';
 listing = dir(dbFolder);
-
 for i = 1:numel(listing)
     if strcmp(listing(i).name, '.') || strcmp(listing(i).name, '..')
         continue;
@@ -17,12 +16,27 @@ for i = 1:numel(listing)
     % Generate the image filename & load image
     imgFilename = strcat(dbFolder, '/', listing(i).name);
     img = imread(imgFilename);
+    
     if ~ismatrix(img)
         img = rgb2gray(img);
     end
-    img = imsharpen(img, 'Threshold', 0.25, 'Amount', 2, 'Radius', 2);
-    img = normalizeImg(img);
     
+
+    img = imsharpen(img, 'Threshold', 0.3, 'Amount', 2, 'Radius', 2);
+    img = normalizeImg(img);
+    %%
+    %find the center
+    %[centers, radii] = imfindcircles(img,[50 length(img)],'ObjectPolarity','bright', ...
+    %'Sensitivity',0.92, 'EdgeThreshold',0.2);
+    %if ~isempty(centers)
+    %    yRange = floor(centers(1))-floor(radii(1)):floor(centers(1))+floor(radii(1));
+    %    xRange = floor(centers(2))-floor(radii(1)):floor(centers(2))+floor(radii(1));
+    %    img = img(xRange,yRange,:);
+    %end
+    %%
+    %turn the image into a binary image
+    %img = imbinarize(img, .7);
+    %%
     fprintf('Loading image %s into database\n', imgFilename);
     
     % Detect features from the image
@@ -32,7 +46,11 @@ for i = 1:numel(listing)
     % Instantiate the quarter object & add to database
     curQuarter = Quarter(stateName, img, features, valid_points);
     db = db.addQuarter(stateName, curQuarter);
+    imshow(img)
+    hold on; plot(points); hold off;
+    
 end
+
 
 fprintf('Added %d quarter(s) to the database\n', db.numQuarters());
 
--- detectStateQuarter.m
+++ detectStateQuarter.m
@@ -7,8 +7,18 @@ if ~ismatrix(img)
     img = rgb2gray(img);
 end
 img = normalizeImg(img);
+%img = imbinarize(img, .85);
+%[centers, radii] = imfindcircles(img,[25 length(img)],'ObjectPolarity','bright', ...
+%    'Sensitivity',0.92, 'EdgeThreshold',0.2);
+%    if ~isempty(centers)
+%        yRange = floor(centers(1))-floor(radii(1)):floor(centers(1))+floor(radii(1));
+%        xRange = floor(centers(2))-floor(radii(1)):floor(centers(2))+floor(radii(1));
+%        img = img(xRange,yRange,:);
+%    end
 points = detector.detectFeatures(img);
 [features, points] = extractFeatures(img, points);
+imshow(img)
+hold on; plot(points); hold off;
 
 % Compare image to those in the database to see how well they match
 keys = db.getKeys();
@@ -16,9 +26,9 @@ minDistance = Inf;
 state = 'n/a';
 for i = 1:numel(keys)
     curQuarter = db.getQuarter(keys{i});
-    [indexPairs, distances] = matchFeatures(features, ...
-        curQuarter.features);
-    
+    cqFeatures = curQuarter.features;
+    %[indexPairs, distances] = matchFeatures(features, qFeatures, 'Method', 'Exhaustive', 'MaxRatio', .4);
+    [indexPairs, distances] = matchFeatures(features, cqFeatures);    
     if size(indexPairs, 1) > 0
         avgDistance = mean(abs(distances));
         if avgDistance < minDistance
