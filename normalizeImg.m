c4c4eee57db68189d5c1459a8387c06dc3e0573c
rlyerly <rlyerly@vt.edu>
2016-12-08 11:11:37 -0500

    Normalizing images to a set size

--- normalizeImg.m
+++ normalizeImg.m
@@ -1,8 +1,12 @@
 function nImg = normalizeImg(img)
 %NORMALIZEIMG Normalize image
 
-scale = 800 / size(img, 1);
-nImg = mat2gray(imresize(img, scale));
+if ~ismatrix(img)
+    img = rgb2gray(img);
+end
+%scale = 800 / size(img, 1);
+img = imresize(img, [800 800]);
+nImg = imsharpen(img, 'Radius', 11, 'Amount', 1.7);
 
 end
 
