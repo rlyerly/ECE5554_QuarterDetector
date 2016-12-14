5dc468b1ddfc9507a53cf7ea3b6bdfe524222c57
laurenwong94 <lwong@oberlin.edu>
2016-12-14 11:49:19 -0500

    Update videoDetector.m

--- videoDetector.m
+++ videoDetector.m
@@ -1,11 +1,35 @@
 close all;
 clear all;
 
-% this will have to be altered to run on someone elses computer
-snapshot = zeros(100, 720, 1280,3);
-vidobj = videoinput('macvideo', 1, 'YCbCr422_1280x720');
-%
+HOGCellSize = [16 16];
+if exist(trainingDataFile, 'file') == 2
+    load(trainingDataFile);
+    assert(exist('trainingFeatures', 'var') == 1, 'No training features');
+    assert(exist('trainingLabels', 'var') == 1, 'No training labels');
+    assert(exist('stateClasses', 'var') == 1, 'No state -> class map');
+    fprintf('Loaded training data (found %d images)\n', ...
+        numel(trainingLabels));
+    
+states = {'alabama', 'alaska', 'arizona', 'arkansas', 'california', ...
+        'colorado', 'connecticut', 'delaware', 'florida', 'georgia', ...
+        'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', ...
+        'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', ...
+        'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', ...
+        'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', ...
+        'newyork', 'normalback', 'normalfront', 'northcarolina', ...
+        'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', ...
+        'rhodeisland', 'southcarolina', 'southdakota', 'statefront', 'tennessee', ...
+        'texas', 'utah', 'vermont', 'virginia', 'washington', ...
+        'westvirginia', 'wisconsin', 'wyoming'};
 
+cameras = imaqhwinfo;
+c = cameras.InstalledAdaptors{end};
+info = imaqhwinfo(c);
+id = info.DeviceIds{end};
+c_info = imaqhwinfo(c,id);
+
+snapshot = zeros(100, 720, 1280,3);
+vidobj = videoinput(c, c_info.DeviceID, c_info.DeviceFormat);
 set(vidobj, 'ReturnedColorSpace', 'RGB');
 triggerconfig(vidobj, 'manual');
 start(vidobj);
@@ -19,7 +43,13 @@ delete(vidobj);
 
 index = randi(10,1,[1,100]);
 snapshot = snapshot(index);
+class = zeros(10,1);
+for ii = 1:10
+   img = normalizeImg(snapshot(ii)); 
+   features = extractHOGFeatures(img, 'CellSize', HOGCellSize);
+   class(ii) = predict(SVMClassifier, features);
+end
+prediction = mode(class);
 
-%perform transformations on snapshots
-
-%do classification
+imagesc(snapshot(10));
+title(states{prediction});
