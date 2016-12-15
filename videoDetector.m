60610463d57b377514b03e97375dcde6dbb8d0b2
Walker Sensabaugh <walkersens@gmail.com>
2016-12-15 12:36:54 -0500

    Loaded in the classes from trainingData, we were missing a few in the states structure.

--- videoDetector.m
+++ videoDetector.m
@@ -2,28 +2,15 @@
 %clear all;
 trainingDataFile = 'svmTrainingData.mat';
 SVMClassifierFile = 'svmClassifier.mat';
-%SVMClassifier = load(SVMClassifierFile);
-%SVMClassifier = SVMClassifier.SVMClassifier
+
+
+trainingData = load('svmTrainingData.mat');
+states = keys(trainingData.stateClasses);
+clear trainingData;
+SVMClassifier = load(SVMClassifierFile);
+SVMClassifier = SVMClassifier.SVMClassifier
 HOGCellSize = [16 16];
-%if exist(trainingDataFile, 'file') == 2
-    load(trainingDataFile);
-    assert(exist('trainingFeatures', 'var') == 1, 'No training features');
-    assert(exist('trainingLabels', 'var') == 1, 'No training labels');
-    assert(exist('stateClasses', 'var') == 1, 'No state -> class map');
-    fprintf('Loaded training data (found %d images)\n', ...
-        numel(trainingLabels));
-   
-states = {'alabama', 'alaska', 'arizona', 'arkansas', 'california', ...
-        'colorado', 'connecticut', 'delaware', 'florida', 'georgia', ...
-        'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', ...
-        'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', ...
-        'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', ...
-        'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', ...
-        'newyork', 'normalback', 'normalfront', 'northcarolina', ...
-        'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', ...
-        'rhodeisland', 'southcarolina', 'southdakota', 'statefront', 'tennessee', ...
-        'texas', 'utah', 'vermont', 'virginia', 'washington', ...
-        'westvirginia', 'wisconsin', 'wyoming'};
+  
 
 cameras = imaqhwinfo;
 c = cameras.InstalledAdaptors{end};
